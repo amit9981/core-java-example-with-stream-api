1f527844d12c1ba6eb63c1796f85b20367762fec
hemantjava <hemantjava90@gmail.com>
2020-09-25 09:54:51 +0530

    Skipping last elements from list.

--- src/test/java/com/example/corejavaexamplewithstreamapi/interview/ListOperationTest.java
+++ src/test/java/com/example/corejavaexamplewithstreamapi/interview/ListOperationTest.java
@@ -3,7 +3,9 @@ package com.example.corejavaexamplewithstreamapi.interview;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class ListOperationTest {
@@ -34,4 +36,14 @@ class ListOperationTest {
     final List<Integer> integersUp = ListOperation.removeDuplicateJava8(integerList);
     System.out.println(integersUp);
   }
+
+  @Test
+  void skipLastElements() {
+    List<Integer> integerList = new ArrayList<>(Arrays.asList(2, 4, 4, 2, 5, 9, 76));
+    System.out.println(integerList);
+    final LinkedList<Integer> collect = integerList.stream().limit(integerList.size()-1)
+        .collect(Collectors.toCollection(LinkedList::new));
+    System.out.println(collect);
+  }
+
 }
