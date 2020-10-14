9e6b384d5b51610c991084dcda94d6ea16b03cdc
hemantjava <hemantjava90@gmail.com>
2020-10-14 12:11:04 +0530

    Comparing.

--- src/main/java/com/example/corejavaexamplewithstreamapi/interview/dto/Student.java
+++ src/main/java/com/example/corejavaexamplewithstreamapi/interview/dto/Student.java
@@ -1,62 +1,60 @@
 package com.example.corejavaexamplewithstreamapi.interview.dto;
 
 
-public class Student   {
+public class Student {
 
-  private Integer studentID;
-  private String firstName;
-  private String fastName;
-  private Integer mark;
+    private Integer studentID;
+    private String firstName;
+    private String lastName;
+    private Integer mark;
 
-  public Student(Integer studentID, String firstName, String fastName, Integer mark) {
-    this.studentID = studentID;
-    this.firstName = firstName;
-    this.fastName = fastName;
-    this.mark = mark;
-  }
+    public Student (Integer studentID, String firstName, String lastName, Integer mark) {
+        this.studentID = studentID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mark = mark;
+    }
 
-  public Integer getStudentID() {
-    return studentID;
-  }
+    @Override
+    public String toString ( ) {
+        return "Student{" +
+                "studentID=" + studentID +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", mark=" + mark +
+                '}';
+    }
 
-  public void setStudentID(Integer studentID) {
-    this.studentID = studentID;
-  }
+    public Integer getStudentID ( ) {
+        return studentID;
+    }
 
-  public String getFirstName() {
-    return firstName;
-  }
+    public void setStudentID (Integer studentID) {
+        this.studentID = studentID;
+    }
 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
+    public String getFirstName ( ) {
+        return firstName;
+    }
 
-  public String getFastName() {
-    return fastName;
-  }
+    public void setFirstName (String firstName) {
+        this.firstName = firstName;
+    }
 
-  public void setFastName(String fastName) {
-    this.fastName = fastName;
-  }
+    public String getLastName ( ) {
+        return lastName;
+    }
 
-  public Integer getMark() {
-    return mark;
-  }
+    public void setLastName (String lastName) {
+        this.lastName = lastName;
+    }
 
-  public void setMark(Integer mark) {
-    this.mark = mark;
-  }
+    public Integer getMark ( ) {
+        return mark;
+    }
 
-  @Override
-  public String toString() {
-    return "Student{" +
-        "studentID=" + studentID +
-        ", firstName='" + firstName + '\'' +
-        ", fastName='" + fastName + '\'' +
-        ", mark=" + mark +
-        '}';
-  }
-
-
-
+    public void setMark (Integer mark) {
+        this.mark = mark;
+    }
 }
+
--- src/main/java/com/example/corejavaexamplewithstreamapi/interview/service/SortListByMarks.java
+++ src/main/java/com/example/corejavaexamplewithstreamapi/interview/service/SortListByMarks.java
@@ -3,43 +3,19 @@ package com.example.corejavaexamplewithstreamapi.interview.service;
 
 import com.example.corejavaexamplewithstreamapi.interview.dto.Student;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class SortListByMarks {
 
-  private static List<Student> studentList;
-
-  static {
-    studentList = Arrays.asList(new Student(121, "hemant", "sahu", 45),
-        new Student(129, "sahu", "sahu", 46),
-        new Student(120, "chitresh", "sahu", 33),
-        new Student(101, "sarita", "sahu", 50),
-        new Student(111, "shaw", "sahu", 50)
-        );
-  }
-
-  public static void main(String[] args) {
-    System.out.println(studentList);
-    Collections.sort(studentList,marks);
-    System.out.println(studentList);
-
-  }
- public static Comparator<Student> marks = new Comparator<Student>(){
-
-     @Override
-     public int compare(Student o1, Student o2) {
-       if (o2.getMark() - o1.getMark() > 0)
-         return 1;
-       else if (o2.getMark() - o1.getMark() < 0)
-         return -1;
-       else if (o2.getMark() == o1.getMark())
-         return o2.getFastName().compareTo(o1.getFastName());
-       return 0;
-
-     }
- };
+    public static void sort (List<Student> studentList) {
+        final Comparator<Student> comparingMark = Comparator.comparing(Student::getMark, Comparator.reverseOrder());
+        final Comparator<Student> comparingFistName = Comparator.comparing(Student::getFirstName, Comparator.reverseOrder());
+        final Comparator<Student> comparingLastName = Comparator.comparing(Student::getLastName, Comparator.reverseOrder());
+        final Comparator<Student> studentComparator = comparingMark.thenComparing(comparingFistName)
+                .thenComparing(comparingLastName);
+        Collections.sort(studentList, studentComparator);
+    }
 
 }
