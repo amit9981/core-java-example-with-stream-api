dba68a253d0979ea283e8630d4d738079d484273
hemantjava <hemantjava90@gmail.com>
2020-11-09 22:54:51 +0530

    ImmutableClass.

--- src/test/java/com/example/corejavaexamplewithstreamapi/optional/Example.java
+++ src/test/java/com/example/corejavaexamplewithstreamapi/optional/Example.java
@@ -9,8 +9,23 @@ public class Example {
   void test(){
     //Optional<Long> aLong = Optional.of(null); NPE
     Optional<Long> aLong = Optional.ofNullable(null);
-
     System.out.println(aLong.orElse(0L).toString());
    }
+   @Test
+  void orElseGet(){
+     Optional<Long> aLong = Optional.ofNullable(null);
+     final Long elseGet = aLong.orElseGet(() -> {
+
+       return getLong();
+     });
+     System.out.println(elseGet);
+   }
+@Test
+void orElseThrow(){
+
+}
+  private Long getLong() {
+    return 3L;
+  }
 
 }
