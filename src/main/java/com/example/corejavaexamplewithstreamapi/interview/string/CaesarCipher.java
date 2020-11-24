59ac1e1653889c58111ba0ae7182bd7ee02b69e7
hemantjava <hemantjava90@gmail.com>
2020-11-24 19:37:49 +0530

    Caesar Cipher in Cryptography.

---
+++ src/main/java/com/example/corejavaexamplewithstreamapi/interview/string/CaesarCipher.java
@@ -0,0 +1,42 @@
+package com.example.corejavaexamplewithstreamapi.interview.string;
+
+//A Java Program to illustrate Caesar Cipher Technique
+public class CaesarCipher {
+    // Encrypts text using a shift od s
+    public static StringBuffer encrypt (String text, int s) {
+        StringBuffer result = new StringBuffer();
+
+        for (char c : text.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                char ch = (char) (((int) c +
+                        s - 65) % 26 + 65);
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) c +
+                        s - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+
+
+    public static StringBuffer decrypt (String text, int s) {
+        StringBuffer result = new StringBuffer();
+
+        for (char c : text.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                char ch = (char) (((int) c -
+                        s + 65) % 26 + 65);
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) c -
+                        s + 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+
+
+}
