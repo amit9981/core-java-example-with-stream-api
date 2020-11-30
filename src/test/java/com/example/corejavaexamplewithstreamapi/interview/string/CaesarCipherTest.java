1f0a7cc6ae8ee23a817e9231111378e8a68426f1
hemantjava <hemantjava90@gmail.com>
2020-11-30 13:03:46 +0530

    Short Map by key and value

--- src/test/java/com/example/corejavaexamplewithstreamapi/interview/string/CaesarCipherTest.java
+++ src/test/java/com/example/corejavaexamplewithstreamapi/interview/string/CaesarCipherTest.java
@@ -1,5 +1,7 @@
 package com.example.corejavaexamplewithstreamapi.interview.string;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class CaesarCipherTest {
@@ -12,6 +14,12 @@ class CaesarCipherTest {
         System.out.println("Initial: "+pwd);
         System.out.println("encrypt: "+encrypt);
         System.out.println("decrypt: "+decrypt);
+    }
+
+    @Test
+    void teats(){
+        ((List<Integer>) new ArrayList<Integer>()).add(0);
+        System.out.println(((List<Integer>) new ArrayList<Integer>()).get(0));
     }
 
 
